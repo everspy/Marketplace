3eecd51cbebabf54d7e9dab5b4a7f2b489ab74b9
Matt Brown <gitmattb@gmail.com>
2020-12-02 11:18:05 -0800

    Add categories folder. To be used for custom category filling. Add Generic category and phone category.

---
+++ categories/generic.py
@@ -0,0 +1,34 @@
+
+class Generic:
+
+    def add_photos():
+        #Weird thing. xpath = /html/body/div[1]/div/div[1]/div[1]/div[4]/div/div[2]/div[1]/div/div[3]/div[2]/div[1]/div/div[3]/div[1]/div[2]/div/div/div[2]/div[1]/div/div/div/div/div
+        return
+
+    def title():
+        #Textbox. id = jsc_c_8m / jsc_c_3o
+        return
+
+    def price():
+        #Textbox. id = jsc_c_8o / jsc_c_3q
+        return
+
+    def category():
+        #Dropdown id = jsc_c_8q
+        return
+
+    def condition():
+    	#Dropdown id = jsc_c_8s
+        return
+
+    def description():
+    	#Textbox id = jsc_c_8u
+        return
+
+    def fill_all_sections():
+        add_photos()
+        title()
+        price()
+        category()
+        condition()
+        description()
---
+++ categories/phone.py
@@ -0,0 +1,17 @@
+from generic import Generic
+
+class Phone(Generic):
+
+    def carrier():
+        #Textbox id = jsc_c_98
+        return
+
+    def device_name():
+        #Textbox id = jsc_c_9a
+        return
+
+    def fill_all_sections()
+        super().fill_all_sections()
+        carrier()
+        device_name()
+
