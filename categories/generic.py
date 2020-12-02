2a82986715c4e89d5171cb7cdbaccec477fda1c5
Matt Brown <gitmattb@gmail.com>
2020-12-02 11:47:08 -0800

    Add self to all methods in phone and generic.

--- categories/generic.py
+++ categories/generic.py
@@ -1,34 +1,34 @@
 
 class Generic:
 
-    def add_photos():
+    def add_photos(self):
         #Weird thing. xpath = /html/body/div[1]/div/div[1]/div[1]/div[4]/div/div[2]/div[1]/div/div[3]/div[2]/div[1]/div/div[3]/div[1]/div[2]/div/div/div[2]/div[1]/div/div/div/div/div
         return
 
-    def title():
+    def title(self):
         #Textbox. id = jsc_c_8m / jsc_c_3o
         return
 
-    def price():
+    def price(self):
         #Textbox. id = jsc_c_8o / jsc_c_3q
         return
 
-    def category():
+    def category(self):
         #Dropdown id = jsc_c_8q
         return
 
-    def condition():
+    def condition(self):
     	#Dropdown id = jsc_c_8s
         return
 
-    def description():
+    def description(self):
     	#Textbox id = jsc_c_8u
         return
 
-    def fill_all_sections():
-        add_photos()
-        title()
-        price()
-        category()
-        condition()
-        description()
+    def fill_all_sections(self):
+        self.add_photos()
+        self.title()
+        self.price()
+        self.category()
+        self.condition()
+        self.description()
--- categories/phone.py
+++ categories/phone.py
@@ -2,16 +2,16 @@ from generic import Generic
 
 class Phone(Generic):
 
-    def carrier():
+    def carrier(self):
         #Textbox id = jsc_c_98
         return
 
-    def device_name():
+    def device_name(self):
         #Textbox id = jsc_c_9a
         return
 
-    def fill_all_sections()
+    def fill_all_sections(self)
         super().fill_all_sections()
-        carrier()
-        device_name()
+        self.carrier()
+        self.device_name()
 
